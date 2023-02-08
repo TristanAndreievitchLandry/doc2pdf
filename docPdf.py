2a0132895cddb8a0ae2f54f551a702601195ca1a
Tristan Landry <74570922+TristanAndreievitchLandry@users.noreply.github.com>
2023-02-08 14:44:02 -0500

    doc2pdf

---
+++ docPdf.py
@@ -0,0 +1,6 @@
+from docx2pdf import convert
+
+input = "D:\\Dropbox\\Projets de livres\\Cuisiner le patrimoine\\Textes"
+output = "D:\\Dropbox\\Projets de livres\\Cuisiner le patrimoine\\PDF"
+
+convert(input, output)
